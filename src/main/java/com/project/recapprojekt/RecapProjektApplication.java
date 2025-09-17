85afcd787561372dedc82db35ed3a99b61a94134
Der Portugiese <lyapin.daniel@mm-bbs.de>
2025-09-17 18:52:11 +0200

    Einfügen von javamelody Dependency

---
+++ src/main/java/com/project/recapprojekt/RecapProjektApplication.java
@@ -0,0 +1,12 @@
+package com.project.recapprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecapProjektApplication.class, args);
+    }
+}
